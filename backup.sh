3d18619d9419b4363cb750a7a5ec82e5ea0d6a2f
erant-netology-courses <modnik2009@yandex.ru>
2025-12-13 12:11:13 +0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,18 @@
+# !/bin/sh
+# .gitignore в любом случае
+# 1. Запускать, прокидывая параметры через экспортированные переменные
+# 2. Запустить на месте с прокинутыми переменными
+# 3. Gроинициализировать переменные из соседнего .env через баш скрипт, который запустит этот баш скрипт и прокинет в него переменные
+# 4. Монтировать файл с секретами
+# 5. .env как тут
+now=$(date +"%s_%Y-%m-%d")
+sudo mkdir /opt/backup
+
+docker run --rm \
+  --entrypoint "" \
+  --network=shvirtd-example-python_backend \
+  --env-file .env \
+  -e MYSQL_HOST=db \
+  schnitzler/mysqldump:latest \
+  sh -c 'mysqldump --opt -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" --ssl-mode=DISABLED --default-auth=mysql_native_password' \
+  > /opt/backup/${now}_${MYSQL_DATABASE}.sql 2>&1
